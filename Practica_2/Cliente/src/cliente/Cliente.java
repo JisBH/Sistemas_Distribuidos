a2a4764c9ed2b5fc5568616c6d2be231bbfef9b7
JisBH <soyjesusbh@gmail.com>
2024-05-15 01:11:11 +0200

    Comienzo del proyecto

---
+++ Practica_2/Cliente/src/cliente/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cliente;
+
+import servicio.comun.TLibro;
+
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TLibro tLibro = new TLibro();
+    }
+
+}
